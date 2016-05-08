606629c292ecd0a04c9c7e3a004bb3f1b22b25bc
adachenski <adachenski@aol.com>
2016-05-09 00:16:55 +0300

    Reset Button and UI

--- JavaScript DOM And Events/SimpleCounter/simple-counter.js
+++ JavaScript DOM And Events/SimpleCounter/simple-counter.js
@@ -1,20 +1,41 @@
 /**
  * Created by Just Nasko on 5/8/2016.
  */
-var count = 0;
+var count = 1,
+    btn = document.getElementById('button'),
+    btnTop = document.getElementById('button-top'),
+    innerScreen = document.getElementById('inner-screen');
 
 function playSound() {
     var sound = document.getElementById("audio");
     sound.play();
 }
-function startCount() {
+
+
+btn.addEventListener("click",function() {
 
     var stopCount = document.getElementById("input-stop").value;
-    document.getElementById("inner-screen").innerHTML = count;
+    var button = document.getElementById("button");
+
+    innerScreen.innerHTML = count;
 
     if (count >= stopCount) {
-        document.getElementById("inner-screen").style.backgroundColor = "red";
+        innerScreen.style.backgroundColor = "red";
         playSound();
     }
     return count++;
-}
+
+});
+
+btnTop.addEventListener("click",function(){
+    count=1;
+    innerScreen.innerHTML=0;
+    innerScreen.style.backgroundColor="lightGreen";
+});
+
+
+//tochevent for smartphones
+
+button.addEventListener('touchstart',function(){
+
+});
